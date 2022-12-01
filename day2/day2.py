d8b3c4d4027fda4060226f3cbd676360146878c9
Eric Gilligan <gilligan.eric@gmail.com>
2022-12-01 17:03:07 -0500

    day2 placeholders

---
+++ day2/day2.py
@@ -0,0 +1,21 @@
+"""
+Solution for https://adventofcode.com/2022/day/2
+"""
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="day1",
+        description="Outputs AdventOfCode 2022 Day 2 solution "
+                    "for provided input.",
+    )
+    parser.add_argument("input", help="Input file to process.")
+    args = parser.parse_args()
+
+    with open(args.input) as f:
+        data = [line.rstrip() for line in f]
+
+
+if __name__ == "__main__":
+    main()
