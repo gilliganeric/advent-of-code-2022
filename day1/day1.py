52d543f4646c8f518ecd84e5ecb071bd8491b33a
Eric Gilligan <gilligan.eric@gmail.com>
2022-12-01 16:10:38 -0500

    Implement solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,18 @@
 def main():
-    print("Hello, world!")
+    with open("input/sample.data") as f:
+        data = [line.rstrip() for line in f]
+        
+    elves = []
+    calories_total = 0
+    for el in data:
+        if not el:
+            elves.append(calories_total)
+            calories_total = 0
+            continue
+        calories_total += int(el)
+
+    print(f"The elf carrying the most Calories is carrying {max(elves)} Calories")
+
 
 if __name__=="__main__":
     main()
