05f5b07945d869b4b136f3a19c8f7d4e8472d90c
Eric Gilligan <gilligan.eric@gmail.com>
2022-12-02 11:02:07 -0500

    Part 1 day2

--- day2/day2.py
+++ day2/day2.py
@@ -6,7 +6,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(
-        prog="day1",
+        prog="day2",
         description="Outputs AdventOfCode 2022 Day 2 solution "
                     "for provided input.",
     )
@@ -16,7 +16,35 @@ def main():
     with open(args.input) as f:
         data = [line.rstrip() for line in f]
 
-    print(data)
+    shape_score = {
+        'X': 1,
+        'Y': 2,
+        'Z': 3,
+    }
+    LOSE = 0
+    DRAW = 3
+    WIN = 6
+    outcome_score = {
+        'A X': DRAW,
+        'A Y': WIN,
+        'A Z': LOSE,
+
+        'B X': LOSE,
+        'B Y': DRAW,
+        'B Z': WIN,
+
+        'C X': WIN,
+        'C Y': LOSE,
+        'C Z': DRAW,
+    }
+
+    total_score = 0
+    for round in data:
+        player = round.split(' ')[1]
+        score = shape_score[player] + outcome_score[round]
+        total_score += score
+
+    print(total_score)
 
 
 if __name__ == "__main__":
