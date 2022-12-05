9985a2179363f575b17da15d29979533fb0ad9ec
Eric Gilligan <gilligan.eric@gmail.com>
2022-12-05 15:06:07 -0500

    day4

--- day4/day4.py
+++ day4/day4.py
@@ -3,6 +3,21 @@ Solution for https://adventofcode.com/2022/day/4
 """
 import argparse
 
+def a_contains_b(a, b):
+    if int(a[0]) <= int(b[0]) and int(a[1]) >= int(b[1]):
+        return True
+    return False
+
+
+def a_overlaps_b(a, b):
+    a_range = range(int(a[0]), int(a[1]) + 1)
+    b_range = range(int(b[0]), int(b[1]) + 1)
+
+    a_set = set(a_range)
+    b_set = set(b_range)
+
+    return not a_set.isdisjoint(b_set)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -18,6 +33,21 @@ def main():
 
     total_part1_val = 0
     total_part2_val = 0
+    for d in data:
+        pair = d.split(",")
+        pair[0] = pair[0].split("-")
+        pair[1] = pair[1].split("-")
+        if a_contains_b(pair[0], pair[1]):
+            total_part1_val += 1
+            print(f"{pair[0]} contains {pair[1]}")
+        elif a_contains_b(pair[1], pair[0]):
+            total_part1_val += 1
+            print(f"{pair[1]} contains {pair[0]}")
+
+        if a_overlaps_b(pair[0], pair[1]):
+            total_part2_val += 1
+        else:
+            print(f"{pair[0]} doesn't overlap {pair[1]}")
 
     print(f"Part1: {total_part1_val}")
     print(f"Part2: {total_part2_val}")
